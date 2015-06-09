213b8c0bd517b55f86776e0fc0afe521e3dabe26
zuuperman <nils.destoop@gmail.com>
2015-06-09 16:31:25 +0200

    HER-28: Allow the searching on other pages

--- angular/app.controller.js
+++ angular/app.controller.js
@@ -12,13 +12,23 @@
     .module('udbApp')
     .controller('AppCtrl', AppController);
 
-  AppController.$inject = ['$scope'];
+  AppController.$inject = ['$scope', 'appConfig'];
 
-  function AppController($scope) {
+  function AppController($scope, appConfig) {
+
+    $scope.searchQuery = '';
     $scope.showJobLog = false;
+    $scope.startSearch = startSearch;
 
     function toggleJobLog() {
       $scope.showJobLog = !$scope.showJobLog;
+    }
+
+    /**
+     * Start a search on drupal.
+     */
+    function startSearch() {
+      window.location.href = '/udb3/search#?query=' + $scope.searchQuery;
     }
 
     $scope.toggleJobLog = toggleJobLog;
