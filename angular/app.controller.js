21001695597c0de71722184ca567ac25767226b7
Nick <nick@2dotstwice.be>
2016-03-14 17:05:58 +0100

    III-820: google tag manager script

--- angular/app.controller.js
+++ angular/app.controller.js
@@ -12,10 +12,25 @@
     .module('udbApp')
     .controller('AppCtrl', AppController);
 
-  AppController.$inject = ['$scope', '$rootScope', '$location'];
+  AppController.$inject = ['$scope', 'appConfig', '$window'];
 
-  function AppController($scope) {
+  function AppController($scope, appConfig, $window) {
     $scope.showJobLog = false;
+
+    // Load Google Tag Manager only when the key is not empty
+    if(appConfig.gaTagManager.containerId) {
+      angular.element(document).ready(function () {
+        (function (w, d, s, l, i) {
+          w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+          var f = d.getElementsByTagName(s)[0],
+              j = d.createElement(s),
+              dl = l !== 'dataLayer' ? '&l=' + l : '';
+          j.async = true;
+          j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
+          f.parentNode.insertBefore(j, f);
+        })($window, document, 'script', 'tm', appConfig.gaTagManager.containerId);
+      });
+    }
 
     function toggleJobLog() {
       $scope.showJobLog = !$scope.showJobLog;
